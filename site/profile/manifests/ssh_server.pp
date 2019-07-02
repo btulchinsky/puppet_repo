5d8b818fed023fadab6c5350d5af6636c452a389
btulchinsky <barry.tulchinsky@gmail.com>
2019-07-01 22:17:52 -0400

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDeinp5iS0j/nCKE2MvczGbC50gC3jJj5J0el5H0Cxq0ZRA58876a/19/YRID5xxdDgMY5+XrH1uQUdgdtnNd2adNAtIC0hHITKnhfTLTnP5dc9t+LM85elM+3BZhfxtREvLR3F0tTqnsoNgG16ktfnsKThsMPstlN2Lha/WEagevI2AWALK2tMJ9sbPRLC24hfZXttL55f3Om4zysOcTuSf/ULrOvvyriS0BZ30qSsKLxda+qKoCPqYt3bfKcpTMfZ7PiADWMthu/WaNFBy04cyRL22n1pWR2azU6zR0DaxTolVeevA2qur0utW04XYj1/wty+sv0PPJSSebfffOnb',
 	}  
 }
