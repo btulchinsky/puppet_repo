bb143aae181fbd1b1e7b5a6513011bdbad0f8e74
btulchinsky <barry.tulchinsky@gmail.com>
2019-07-04 22:48:00 -0400

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -4,7 +4,7 @@ class minecraft {
   }
   file {'/opt/minecraft/minecraft_server.jar':
     ensure => file,
-    source => https://launcher.mojang.com/v1/objects/d0d0fe2b1dc6ab4c65554cb734270872b72dadd6/server.jar,
+    source => 'https://launcher.mojang.com/v1/objects/d0d0fe2b1dc6ab4c65554cb734270872b72dadd6/server.jar',
   }
   package {'java':
     ensure => present,
